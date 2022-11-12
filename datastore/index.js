8dc37bc1fa8d131669d5eca2fb7bae1f2ea6eb7f
gbmyt <gbmytabc1@gmail.com>
2022-11-11 19:56:34 -0500

    partial index.js refactor from internal storage object to array

--- datastore/index.js
+++ datastore/index.js
@@ -3,34 +3,37 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-var items = {};
+var items = []; // { id: 00001, text: 'str' }
 var itemsTest = items;
 exports.itemsTest = itemsTest;
 
-// items = {
+// items = [
 //   { id: '00001', text: 'todo 1' },
 //   { id: '00002', text: 'todo 2' }
-// }
+// ]
 exports.dataDir = path.join(__dirname, 'data');
 
-// Public API - Fix these CRUD functions ///////////////////////////////////////
+// Public API - Fix these CRUD functions /////////////////////////////////////
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
     // console.log('get unique inner cb', id);
-    items['id'] = id;
-    items['text'] = text;
+    var todo = {
+      'id': id,
+      'text': text
+    };
+    items.push(todo);
 
     // console.log('Item ID', items.id);
 
-    var uniquePath = path.join(exports.dataDir, `/${items.id}.txt`);
+    var uniquePath = path.join(exports.dataDir, `/${id}.txt`);
 
     fs.writeFile(uniquePath, text, (err) => {
       if (err) {
         console.log('write err', err);
         callback(err);
       } else {
-        callback(null, { id: items.id, text: text });
+        callback(null, todo);
       }
     });
   });
@@ -43,9 +46,9 @@ exports.readAll = (callback) => {
 
   var fileNames = fs.readdirSync(exports.dataDir);
 
-  var data = _.map(fileNames, (text, index) => {
+  var data = _.map(fileNames, (id, index) => {
     // console.log('text', text.replace('.txt', ''), 'index', index);
-    return { 'id': text.replace('.txt', ''), 'text': text.replace('.txt', '') };
+    return { 'id': id.replace('.txt', ''), 'text': items[index].text };
   });
 
   // console.log('Read Dir Log', data);
@@ -53,23 +56,17 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var paddedNum = items['id'];
-  console.log('items', items);
-  console.log('paddedNum', paddedNum);
+  var paddedNum = id;
 
-  if (!paddedNum) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    var pathName = path.join(exports.dataDir, paddedNum + '.txt');
+  var pathName = path.join(exports.dataDir, paddedNum + '.txt');
 
-    fs.readFile(pathName, (err, data) => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, { 'id': id, 'text': data.toString() });
-      }
-    });
-  }
+  fs.readFile(pathName, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, { 'id': id, 'text': data.toString() }); // needs refactor
+    }
+  });
 };
 
 // exports.update = (id, text, callback) => {
@@ -93,30 +90,37 @@ exports.readOne = (id, callback) => {
 
 // First pass .update refactor (passes test suite)
 exports.update = (id, text, callback) => {
-  var item = items['id'];
-  // console.log('item', item, 'items', items, 'text', text);
+  // var item = items['id'];
+  // // console.log('item', item, 'items', items, 'text', text);
 
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    // items['text'] = text;
-    var filePath = path.join(exports.dataDir, id + '.txt');
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  // items['text'] = text;
+  var filePath = path.join(exports.dataDir, id + '.txt');
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        console.log('Hi :)');
-        callback(err);
-      } else {
-        fs.writeFile(filePath, text, (err, data) => {
-          if (err) {
-            callback(err);
-          } else {
-            callback({ id, text });
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.log('Hi :)');
+      callback(err);
+    } else {
+      fs.writeFile(filePath, text, (err, data) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback({ id, text });
+
+          for (var i of items) {
+            if (i['id'] === id) {
+              i['text'] = text;
+            }
           }
-        });
-      }
-    });
-  }
+
+        }
+      });
+    }
+  });
+  // }
 };
 
 exports.delete = (id, callback) => {
